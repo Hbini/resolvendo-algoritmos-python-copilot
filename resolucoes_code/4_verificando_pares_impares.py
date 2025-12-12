990e138dcde0170b0a52b7518d12063387d353db
Hbini <65453079+Hbini@users.noreply.github.com>
2025-12-12 14:51:46 -0300

    Add script to check if a number is even or odd

---
+++ resolucoes_code/4_verificando_pares_impares.py
@@ -0,0 +1,9 @@
+# 4 - Verificando Números Pares e Ímpares
+# Descrição: Verifica se um número é par ou ímpar.
+
+num = int(input("Digite um número: "))
+
+if num % 2 == 0:
+    print(f"{num} é PAR")
+else:
+    print(f"{num} é ÍMPAR")
